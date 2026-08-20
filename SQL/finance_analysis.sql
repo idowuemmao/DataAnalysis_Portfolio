f147b40518a45165162c3991fb20573a16fe9b38
Idowu Emmanuel <idowuemmao@gmail.com>
2026-08-20 09:02:57 +0100

    Added average discount calculation

--- SQL/finance_analysis.sql
+++ SQL/finance_analysis.sql
@@ -1,7 +1,8 @@
 SELECT 
     productname, regionname,
     SUM(quantity) AS total_qty,
-    ROUND(AVG(revenue),2) AS avg_rev
+    ROUND(AVG(revenue),2) AS avg_rev,
+    AVG(discount_price) AS avg_discount
 FROM sales
 WHERE 
 regionname = "Asia"
